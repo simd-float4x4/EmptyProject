3f5c7acbf49ac0a23720a645198ce11f97cf3020
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-11-27 09:07:00 +0900

    initial commit

---
+++ EmptyProject/EmptyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyProjectApp.swift
+//  EmptyProject
+//
+//  Created by Shumpei Horiuchi on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct EmptyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
